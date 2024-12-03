856b0fba2cd0ddf86f32e236c0c2d0f8f095e73b
lieonlion <lieonlion4@gmail.com>
2024-12-03 20:56:44 +0000

    1.2.10

---
+++ src/main/java/io/github/lieonlion/quad/mixin/PortalShapeMixin.java
@@ -0,0 +1,26 @@
+package io.github.lieonlion.quad.mixin;
+
+import io.github.lieonlion.quad.tags.QuadBlockTags;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.portal.PortalShape;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = PortalShape.class, priority = 1004)
+public abstract class PortalShapeMixin {
+    @Shadow private static @Final @Mutable BlockBehaviour.StatePredicate FRAME;
+
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    private void PortalShape(CallbackInfo ci) {
+        FRAME = (BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) ->
+                blockState.is(QuadBlockTags.NETHER_PORTAL_BUILT);
+    }
+}
---
+++ src/main/java/io/github/lieonlion/quad/registry/QuadFuelRegistry.java
@@ -0,0 +1,28 @@
+package io.github.lieonlion.quad.registry;
+
+import io.github.lieonlion.quad.Quad;
+import io.github.lieonlion.quad.tags.QuadItemTags;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.level.block.entity.FuelValues;
+
+public class QuadFuelRegistry {
+    public static void registerFuel(FuelValues.Builder builder, FuelRegistryEvents.Context context) {
+        int i = context.baseSmeltTime();
+        builder.add(QuadItemTags.FUEL_BAMBOO, i / 4)
+                .add(QuadItemTags.FUEL_CARPET, 1 + i / 3)
+                .add(QuadItemTags.FUEL_WOOL, i / 2)
+                .add(QuadItemTags.FUEL_WOOD_SLAB, i * 3 / 4)
+                .add(QuadItemTags.FUEL_WOOD_TOOL, i)
+                .add(QuadItemTags.FUEL_WOOD, i * 3 / 2)
+                .add(QuadItemTags.FUEL_HANGING_SIGN, i * 4)
+                .add(QuadItemTags.FUEL_BOAT, i * 6)
+                .add(QuadItemTags.FUEL_COAL, i * 8)
+                .add(QuadItemTags.FUEL_BLAZE_ROD, i * 12)
+                .add(QuadItemTags.FUEL_DRIED_KELP_BLOCK, 1 + i * 20)
+                .add(QuadItemTags.FUEL_COAL_BLOCK, i * 8 * 10)
+                .add(QuadItemTags.FUEL_LAVA, i * 100)
+                .remove(ItemTags.NON_FLAMMABLE_WOOD);
+        Quad.LOGGER.info("[Quad] loaded quad fuel tags");
+    }
+}
